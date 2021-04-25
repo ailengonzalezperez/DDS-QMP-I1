be258473be0739bbc09486713f66651011998bbe
Ailén González Pérez <ailengonzalezperez2@gmail.com>
2021-04-25 12:11:48 -0300

    Iteración 2 -  Primera parte requerimientos
    Como usuario de QuéMePongo, quiero especificar qué trama tiene la tela de una prenda (lisa, rayada, con lunares, a cuadros o un estampado). Armo un enum de tramas -> la sumo y pongo el chequeo de tela. Voy a tener una validación: que la prenda tenga trama si es de tela. Sería mejor pensar la tela aparte?
    
    Como usuario de QuéMePongo, quiero crear una prenda especificando primero de qué tipo es. -> lo pongo en el constructor primero
    
    Como usuario de QuéMePongo, quiero crear una prenda especificando en segundo lugar los aspectos relacionados a su material (colores, material, trama, etc) para evitar elegir materiales inconsistentes con el tipo de prenda. ->  Resuelto con lo de arriba
    
    Como usuario de QuéMePongo, quiero guardar un borrador de la la última prenda que empecé a cargar para continuar después. ->  Si es borrador, no va a tener todo -> tengo que poder cargarle los datos por separados
    
    Como usuario de QuéMePongo, quiero poder no indicar ninguna trama para una tela, y que por defecto ésta sea lisa. ->  Pongo trama por defecto lisa chequeando en constructor. Me hace ruido la cantidad de chequeos al construir. Debería poder ponerlo en otro lado
    
    Como usuario de QuéMePongo, quiero poder guardar una prenda solamente si esta es válida. -> Dado por los chequeos de excepciones

--- queMePongo.java
+++ queMePongo.java
@@ -3,33 +3,65 @@ class Prenda{
   Material material; 
   Color colorPrincipal; 
   Color colorSecundario; 
+  Trama trama;
   
-  public Prenda(TipoPrenda tipo, Categoria categoria; Material material; Color colorPrincipal){ 
-    if (tipo.getCategoria() != categoria){
-		throw new PrendaInvalidaException("Categoría inválida");
-    else if (tipo == null || categoria == null || material == null || colorPrincipal == null){
+  public Prenda(TipoPrenda tipo, Categoria categoria; Material material; Trama trama; Color colorPrincipal){ 
+	  
+    if (tipo == null || categoria == null || material == null || colorPrincipal == null){
       throw new PrendaInvalidaException("Faltan datos de la prenda.");
-    }else{
-      this.tipo = tipo;
-      this.material = material;
-      this.colorPrincipal = colorPrincipal;
-    } else { 
-      throw new TipoCategoriaNoValidoException();
+    }
+    
+    if (tipo.getCategoria() != categoria){
+	  throw new PrendaInvalidaException("Categoría inválida");
+	}
+		   
+    if (trama != null && material != Material.TELA){
+        throw new PrendaInvalidaException("Este material no lleva trama");
+    }
+    
+    if (trama == null && material == Material.TELA){ 
+		this.trama = Trama.LISA;
+	}
+	
+	if (trama != null && material == Material.TELA){
+		this.trama = trama;
+	    
+    }
+    
+    this.tipo = tipo;
+    this.material = material;
+    this.colorPrincipal = colorPrincipal;
+      
   }
  
-  public Prenda(TipoPrenda tipo, Categoria categoria; Material material; Color colorPrincipal; Color colorSecundario){ 
-    if (tipo.getCategoria() != categoria){
-		throw new PrendaInvalidaException("Categoría inválida");
-    else if (tipo == null || categoria == null || material == null || colorPrincipal == null){
+  public Prenda(TipoPrenda tipo, Categoria categoria; Material material; Trama trama; Color colorPrincipal; Color colorSecundario){ 
+    if (tipo == null || categoria == null || material == null || colorPrincipal == null){
       throw new PrendaInvalidaException("Faltan datos de la prenda.");
+    }
     
-    }else{
-      this.tipo = tipo;
-      this.material = material;
-      this.colorPrincipal = colorPrincipal;
-      this.colorSecundario = colorSecundario;
-    } 
-  }
+    if (tipo.getCategoria() != categoria){
+	  throw new PrendaInvalidaException("Categoría inválida");
+	}
+		   
+    if (trama != null && material != Material.TELA){
+        throw new PrendaInvalidaException("Este material no lleva trama");
+    }
+    
+    if (trama == null && material == Material.TELA){ 
+		this.trama = Trama.LISA;
+	}
+	
+	if (trama != null && material == Material.TELA){
+		this.trama = trama;
+	    
+    }
+    
+    this.tipo = tipo;
+    this.material = material;
+    this.colorPrincipal = colorPrincipal;
+    this.colorSecundario = colorSecundario;
+   }
+
    
   public Categoria getCategoria(){
 	return this.tipo.getCategoria();
@@ -61,13 +93,48 @@ enum TipoPrenda{
   PANTALON(Categoria.PARTEINFERIOR)
   
 }
+ 
+class Color{  //Clase sin comportamiento pero vale la pena por la abstracción
+  Int rojo;
+  Int verde;
+  Int azul;
+}
     
 enum Material{
   JEAN, CUERO, TELA
 }
 
-class Color{  //Clase sin comportamiento pero vale la pena por la abstracción
-  Int rojo;
-  Int verde;
-  Int azul;
+enum Trama{
+	LISA, RAYADA, LUNARES, CUADROS, ESTAMPADO
+}
+
+class BorradorPrenda{
+  TipoPrenda tipo; 
+  Material material; 
+  Color colorPrincipal; 
+  Color colorSecundario; 
+  Trama trama = Trama.LISA;
+  
+  setTipo(TipoPrenda tipo){
+    this.tipo = tipo;
+  }
+  
+  setTipo(Material material){
+    this.material = material;
+  }
+  
+  setTipo(Color colorPrincipal;){
+    this.colorPrincipal = colorPrincipal;
+  }
+  
+  setTipo(Color colorSecundario){
+    this.colorSecundario = colorSecundario;
+  }
+  
+  setTipo(Trama trama){
+    this.trama = trama;
+  }
+
+  Prenda generarPrenda(){
+	  return Prenda(...)
 }
